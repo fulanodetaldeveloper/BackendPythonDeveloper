3f3777d622d6b43fa6e4ab161dd6f1d7631ab2e2
Francisco <francisco@mimail.com>
2024-04-28 23:54:13 -0600

    fix: Recurso /asignaciones, se quito validacion de datos inecesaria en este proceso, ya que su funcionalidad es listar los grimorios asignados, sin esperar ningun dato en el request

--- src/routes/asignacion_grimorio_routes.py
+++ src/routes/asignacion_grimorio_routes.py
@@ -6,7 +6,5 @@ api_asignacion_grimorios_bp = Blueprint('asignacion_grimorios_route', __name__)
 
 @api_asignacion_grimorios_bp.route('/', methods=['GET'])
 def get_asignacion_grimorios():
-    if not request.is_json:
-        return jsonify({'message': 'Invalid JSON'}), 400
     
     return  AsignacionService.list_asignacion_grimorios()
